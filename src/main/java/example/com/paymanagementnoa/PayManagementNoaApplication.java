f5687134ddf1383b4951c8685c8b174e38cc590d
KimMinHwan <2021111977@dgu.ac.kr>
2026-02-12 20:45:03 +0900

    "FEAT" : "프로젝트 생성"

---
+++ src/main/java/example/com/paymanagementnoa/PayManagementNoaApplication.java
@@ -0,0 +1,13 @@
+package example.com.paymanagementnoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayManagementNoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayManagementNoaApplication.class, args);
+    }
+
+}
