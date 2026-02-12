ae687e657cb2f16f5cf258ba87dfe1640476e7a2
KimMinHwan <2021111977@dgu.ac.kr>
2026-02-12 20:45:03 +0900

    "FEAT" : "프로젝트 생성"

---
+++ src/test/java/example/com/paymanagementnoa/PayManagementNoaApplicationTests.java
@@ -0,0 +1,13 @@
+package example.com.paymanagementnoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayManagementNoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
